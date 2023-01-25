6e082cdf2af61d6c1687c5aa0b86cdad96029021
Sergey <bibert.shmibert@gmail.com>
2023-01-25 15:22:05 +0400

    task_1

--- task1.java
+++ task1.java
@@ -1,7 +1,23 @@
-/**
- * task1
- */
+import java.util.Scanner;
 public class task1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите число: ");
+        boolean flag = iScanner.hasNextInt();
+        int u_value = iScanner.nextInt();
+        iScanner.close();
 
+        int summ_count = 0;
+        int factorial_count = 1;
+        for(int i = summ_count; i <= u_value; i++) {
+            summ_count += i;
+        }
+        System.out.println("Сумма чисел от 1 до " + u_value + " = " + summ_count);
+
+        for(int i = factorial_count; i <= u_value; i++) {
+            factorial_count *= i;
+        }
+        System.out.println("n! = " + factorial_count);
+    }
     
 }
